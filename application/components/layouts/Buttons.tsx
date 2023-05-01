f225a532d450b42a6f81cce701a5e6e32fd07bc8
YeonsuBaek <soothbugger@gmail.com>
2023-05-02 07:47:30 +0900

    style: Buttons 레이아웃 생성

---
+++ application/components/layouts/Buttons.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '../UI/Button';
+
+const Buttons = () => {
+  return (
+    <div className='absolute bottom-[20%] left-1/2 translate-x-[-50%] w-full px-9'>
+      <Button text='네컷사진 만들기' color='#fff' bg='#4A9D93' />
+      <Button text='프레임 구경하기' color='#316962' bg='#B6D7D3' />
+    </div>
+  );
+};
+
+export default Buttons;
