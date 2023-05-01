b5183e849e74d66625b66b2f80502c29ef0a2048
YeonsuBaek <soothbugger@gmail.com>
2023-05-02 08:02:18 +0900

    feat: 메인페이지 구현

--- application/pages/index.tsx
+++ application/pages/index.tsx
@@ -1,5 +1,13 @@
+import Buttons from '@/components/layouts/Buttons';
+import Title from '@/components/Assets/Title';
+
 function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className='relative w-screen h-screen'>
+      <Title />
+      <Buttons />
+    </div>
+  );
 }
 
 export default Home;
