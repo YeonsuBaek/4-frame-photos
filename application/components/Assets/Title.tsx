3cccafa824cb8398eade51897e04747168eb3211
YeonsuBaek <soothbugger@gmail.com>
2023-05-02 07:49:33 +0900

    style: Title 에셋 생성

---
+++ application/components/Assets/Title.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <h1 className='absolute top-[40%] left-1/2 translate-x-[-50%] text-[#2F5B59] text-2xl text-center'>
+      내 손 안에<strong className='block text-5xl'>네컷사진</strong>
+    </h1>
+  );
+};
+
+export default Title;
