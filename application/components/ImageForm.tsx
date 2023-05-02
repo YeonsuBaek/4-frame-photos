576a96c255f905cafaad6ced11353e7e377382af
yeonsuBaek <soothbugger@gmail.com>
2023-05-02 20:40:44 +0900

    feat: ImageForm에 이미지 삽입 기능 구현

--- application/components/ImageForm.tsx
+++ application/components/ImageForm.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import Image from 'next/image';
+import React, { useRef, useState } from 'react';
 
-const ImageForm = () => {
+const ImageForm: React.FC = () => {
+  const [uploadedImage, setUploadedImage] = useState('');
+  const fileInput = useRef<HTMLInputElement>(null);
+
+  const handleUploadImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.currentTarget;
+    const file = (target.files as FileList)[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    return new Promise<void>((resolve) => {
+      reader.onload = () => {
+        const result = reader.result as string;
+        setUploadedImage(result);
+        resolve();
+      };
+    });
+  };
+
   return (
-    <button className='w-full aspect-[3/2] bg-white object-cover'>
-      <input accept='image/*' type='file' className='w-full h-full' />
-    </button>
+    <div>
+      {uploadedImage ? (
+        <img
+          src={uploadedImage}
+          onClick={() => {
+            fileInput.current.click();
+          }}
+          className='w-full aspect-[3/2] bg-white object-cover'
+        />
+      ) : (
+        <input
+          accept='image/*'
+          type='file'
+          onChange={handleUploadImage}
+          ref={fileInput}
+          className='w-full aspect-[3/2] bg-white object-cover'
+        />
+      )}
+    </div>
   );
 };
 
