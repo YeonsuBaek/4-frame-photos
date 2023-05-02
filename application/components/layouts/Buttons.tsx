d24dcf362260d7840fc73b3e6b0e70eb59cadead
YeonsuBaek <soothbugger@gmail.com>
2023-05-02 17:44:15 +0900

    feat: making 페이지 생성

--- application/components/layouts/Buttons.tsx
+++ application/components/layouts/Buttons.tsx
@@ -4,7 +4,7 @@ import Button from '../UI/Button';
 const Buttons = () => {
   return (
     <div className='absolute bottom-[20%] left-1/2 translate-x-[-50%] w-full px-9'>
-      <Button text='네컷사진 만들기' color='#fff' bg='#4A9D93' link='/' />
+      <Button text='네컷사진 만들기' color='#fff' bg='#4A9D93' link='/making' />
       <Button text='프레임 구경하기' color='#316962' bg='#B6D7D3' link='/' />
     </div>
   );
---
+++ application/pages/making/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../../components/layouts/Layout';
+import Optionbar from '../../components/layouts/Optionbar';
+
+const MakingPage = () => {
+  return (
+    <>
+      <Layout title='네컷사진 만들기'>
+        <div className='h-[1000px]'>making</div>
+      </Layout>
+      <Optionbar />
+    </>
+  );
+};
+
+export default MakingPage;
