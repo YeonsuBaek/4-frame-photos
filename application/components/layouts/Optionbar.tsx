94439ade1e7cfbb683b099de8b1a51719adba3d9
yeonsuBaek <soothbugger@gmail.com>
2023-05-02 19:31:44 +0900

    style: Optionbar 버튼 스타일 부여

--- application/components/layouts/Optionbar.tsx
+++ application/components/layouts/Optionbar.tsx
@@ -1,29 +1,57 @@
 import React from 'react';
 import { TbFrame, TbPencilMinus } from 'react-icons/tb';
-import { AiOutlineFontSize, AiOutlineFontColors } from 'react-icons/ai';
+import {
+  AiOutlineFontSize,
+  AiOutlineFontColors,
+  AiOutlineCalendar,
+} from 'react-icons/ai';
 import { RxFontStyle } from 'react-icons/rx';
 import { TiWeatherPartlySunny } from 'react-icons/ti';
 
 const Optionbar = () => {
   return (
-    <ul className='fixed bottom-0 left-0 w-full flex items-center justify-start bg-black'>
-      <li>
-        <TbFrame size={32} color='#fff' />
+    <ul className='fixed bottom-0 left-0 flex items-center justify-start w-screen overflow-scroll text-white bg-black'>
+      <li className='min-w-[80px] flex items-center justify-center'>
+        <button className='flex flex-col items-center justify-center'>
+          <TbFrame size={32} color='#fff' />
+          <span>프레임</span>
+        </button>
       </li>
-      <li>
-        <TbPencilMinus size={32} color='#fff' />
+      <li className='min-w-[80px] flex items-center justify-center'>
+        <button className='flex flex-col items-center justify-center'>
+          <TbPencilMinus size={32} color='#fff' />
+          <span>글자 편집</span>
+        </button>
       </li>
-      <li>
-        <AiOutlineFontSize size={32} color='#fff' />
+      <li className='min-w-[80px] flex items-center justify-center'>
+        <button className='flex flex-col items-center justify-center'>
+          <AiOutlineFontSize size={32} color='#fff' />
+          <span>글자 크기</span>
+        </button>
       </li>
-      <li>
-        <AiOutlineFontColors size={32} color='#fff' />
+      <li className='min-w-[80px] flex items-center justify-center'>
+        <button className='flex flex-col items-center justify-center'>
+          <AiOutlineFontColors size={32} color='#fff' />
+          <span>글자 색상</span>
+        </button>
       </li>
-      <li>
-        <RxFontStyle size={32} color='#fff' />
+      <li className='min-w-[80px] flex items-center justify-center'>
+        <button className='flex flex-col items-center justify-center'>
+          <RxFontStyle size={32} color='#fff' />
+          <span>글씨체</span>
+        </button>
       </li>
-      <li>
-        <TiWeatherPartlySunny size={32} color='#fff' />
+      <li className='min-w-[80px] flex items-center justify-center'>
+        <button className='flex flex-col items-center justify-center'>
+          <AiOutlineCalendar size={32} color='fff' />
+          <span>날짜</span>
+        </button>
+      </li>
+      <li className='min-w-[80px] flex items-center justify-center'>
+        <button className='flex flex-col items-center justify-center'>
+          <TiWeatherPartlySunny size={32} color='#fff' />
+          <span>날씨</span>
+        </button>
       </li>
     </ul>
   );
