697b718ac9033b2b62009f74c502a2326b3c051c
yeonsuBaek <soothbugger@gmail.com>
2023-05-04 16:22:46 +0900

    style: DetailOptionbar 스타일 수정

--- application/components/UI/DetailOptionbar.tsx
+++ application/components/UI/DetailOptionbar.tsx
@@ -9,9 +9,9 @@ const DetailOptionbar: React.FC<{
   onSave: () => void;
 }> = (props) => {
   return (
-    <div className='fixed bottom-0 left-0 z-50 flex flex-col items-center justify-start w-screen h-[72px] overflow-scroll text-white bg-black'>
-      {props.children}
-      <div className='flex justify-between w-full px-4'>
+    <div className='fixed bottom-0 left-0 z-50 flex flex-col items-center justify-start w-screen overflow-scroll text-white bg-black'>
+      <div className='h-[48px] flex items-center'>{props.children}</div>
+      <div className='flex justify-between items-center w-full px-4 h-[40px]'>
         <button onClick={props.onClose}>
           <TfiClose />
         </button>
