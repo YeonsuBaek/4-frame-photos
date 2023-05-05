c09013b109afa97be379465dbd0415911f63c958
YeonsuBaek <soothbugger@gmail.com>
2023-05-05 15:51:27 +0900

    style: DetailOptionbar 버튼 아이콘 변경

--- application/components/UI/DetailOptionbar.tsx
+++ application/components/UI/DetailOptionbar.tsx
@@ -1,18 +1,31 @@
 import DETAIL_OPTIONBAR from '@/models/detailoptionbar';
 import React from 'react';
-import { TfiClose, TfiCheck } from 'react-icons/tfi';
 
 const DetailOptionbar: React.FC<DETAIL_OPTIONBAR> = (props) => {
   return (
     <div className='fixed bottom-0 left-0 z-50 flex flex-col items-center justify-start w-screen overflow-scroll text-white bg-black'>
       <div className='h-[48px] flex items-center w-full'>{props.children}</div>
-      <div className='flex justify-between items-center w-full px-4 h-[40px]'>
-        <button onClick={props.onClose}>
-          <TfiClose />
+      <div className='flex justify-between items-center w-full px-[8px] h-[40px]'>
+        <button
+          onClick={props.onClose}
+          className='w-[40px] h-[40px] flex items-center justify-center'
+        >
+          <img
+            src='/assets/close.png'
+            alt={`${props.title} 옵션 취소`}
+            className='w-[24px] h-[24px]'
+          />
         </button>
         <h3>{props.title}</h3>
-        <button onClick={props.onSave}>
-          <TfiCheck />
+        <button
+          onClick={props.onSave}
+          className='w-[40px] h-[40px] flex items-center justify-center'
+        >
+          <img
+            src='/assets/check.png'
+            alt={`${props.title} 옵션 적용`}
+            className='w-[24px] h-[24px]'
+          />
         </button>
       </div>
     </div>
