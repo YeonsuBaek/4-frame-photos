6264b9a601e40d029266a9312b6eec08dd989cea
YeonsuBaek <soothbugger@gmail.com>
2023-05-06 19:02:52 +0900

    fix: 이미지 저장 기능 수정

--- application/pages/making/index.tsx
+++ application/pages/making/index.tsx
@@ -17,15 +17,13 @@ const MakingPage = () => {
   const handleSaveImage = async (): Promise<void> => {
     const ref: any = document.getElementById('photo');
     await setScale('100%');
-    await html2canvas(ref, {
-      logging: true,
-      allowTaint: true,
-      useCORS: true,
-    }).then((canvas: HTMLCanvasElement) => {
+    await html2canvas(ref).then((canvas: HTMLCanvasElement) => {
       const link = document.createElement('a');
-      link.href = canvas.toDataURL('image/jpeg');
-      link.download = '네컷사진.jpg';
+      document.body.appendChild(link);
+      link.href = canvas.toDataURL('image/png');
+      link.download = '네컷사진.png';
       link.click();
+      document.body.removeChild(link);
     });
   };
 
