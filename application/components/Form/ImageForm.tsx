159aa976b0140b47e777d323318b674688bfe31c
yeonsuBaek <soothbugger@gmail.com>
2023-05-11 17:29:28 +0900

    fix: heic 이미지도 입력 가능

--- application/components/Form/ImageForm.tsx
+++ application/components/Form/ImageForm.tsx
@@ -6,48 +6,32 @@ const ImageForm = () => {
   const fileInput = useRef<any>(null);
 
   const handleUploadImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
+    const reader = new FileReader();
+    let file = fileInput.current.files[0];
 
-    if (file && file.type.startsWith('image/')) {
-      const blob = await readFileAsBlob(file);
-      const pngBlob = await convertBlobToPng(blob);
-      const convertedUrl = URL.createObjectURL(pngBlob);
-
-      setImage(convertedUrl);
-    }
-  };
-
-  const readFileAsBlob = (file: File): Promise<Blob> => {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-
-      reader.onload = (e: ProgressEvent<FileReader>) => {
-        const blob = new Blob([e.target?.result as ArrayBuffer], {
-          type: file.type,
-        });
-        resolve(blob);
-      };
-
-      reader.onerror = () => {
-        reject(new Error('Failed to read the file.'));
-      };
-
-      reader.readAsArrayBuffer(file);
-    });
-  };
-
-  const convertBlobToPng = (blob: Blob): Promise<Blob> => {
-    return new Promise((resolve, reject) => {
-      heic2any({
-        blob: blob,
-        toType: 'image/png',
-      })
-        .then((convertedBlob: any) => {
-          resolve(convertedBlob);
+    if (file.name.split('.')[1] === 'heic') {
+      let blob = fileInput.current.files[0];
+      heic2any({ blob: blob, toType: 'image/jpeg' })
+        .then((resultBlob: any) => {
+          file = new File([resultBlob], file.name.split('.')[0] + '.jpg', {
+            type: 'image/jpeg',
+            lastModified: new Date().getTime(),
+          });
+          reader.readAsDataURL(file);
         })
-        .catch(() => {
-          reject(new Error('Failed to convert the image.'));
+        .catch((error) => {
+          console.error(error);
         });
+    } else {
+      reader.readAsDataURL(file);
+    }
+
+    return new Promise<void>((resolve) => {
+      reader.onload = () => {
+        const result = reader.result as string;
+        setImage(result);
+        resolve();
+      };
     });
   };
 
@@ -62,6 +46,7 @@ const ImageForm = () => {
       />
       <input
         type='file'
+        accept='.heic, image/*'
         onChange={handleUploadImage}
         ref={fileInput}
         className='block w-full aspect-[3/2] object-cover bg-white cursor-pointer'
